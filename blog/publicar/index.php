e40b5a36354456d54fb4d0a69a2cce14196ddcb9
Uhalace <uhalacesouza@gmail.com>
2025-03-19 00:18:40 -0300

    Add files via upload

---
+++ blog/publicar/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/d/d1/Brasão_dos_Estados_Unidos_do_Brazil_%281889%29.svg">
+   <title>Publicar</title>
+   <script type="text/javascript">
+      
+
+
+
+
+   </script>
+    <script src="../js/tinymce.min.js" ></script >
+     <script src="../js/langs/pt_PT.js" ></script >
+  <script >
+     tinymce.init({
+  selector: 'textarea#full-featured-non-premium', 
+   language: 'pt_PT',
+  language_url : '../js/langs/pt_PT.js',
+  plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+  imagetools_cors_hosts: ['picsum.photos'],
+  menubar: 'file edit view insert format tools table help',
+  toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+  toolbar_sticky: true,
+  autosave_ask_before_unload: true,
+  autosave_interval: "30s",
+  autosave_prefix: "{path}{query}-{id}-",
+  autosave_restore_when_empty: false,
+  autosave_retention: "2m",
+  image_advtab: true,
+  content_css: '//www.tiny.cloud/css/codepen.min.css',
+  link_list: [
+    { title: 'My page 1', value: 'http://www.tinymce.com' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_list: [
+    { title: 'My page 1', value: 'http://www.tinymce.com' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_class_list: [
+    { title: 'None', value: '' },
+    { title: 'Some class', value: 'class-name' }
+  ],
+  importcss_append: true,
+  file_picker_callback: function (callback, value, meta) {
+    /* Provide file and text for the link dialog */
+    if (meta.filetype === 'file') {
+      callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+    }
+
+    /* Provide image and alt text for the image dialog */
+    if (meta.filetype === 'image') {
+      callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+    }
+
+    /* Provide alternative source and posted for the media dialog */
+    if (meta.filetype === 'media') {
+      callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+    }
+  },
+  templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+  ],
+  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+  height: 520,
+  image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  noneditable_noneditable_class: "mceNonEditable",
+  toolbar_mode: 'sliding',
+  contextmenu: "link image imagetools table",
+ });
+  </script >
+  <link rel="stylesheet" type="text/css" href="https://www.tiny.cloud/css/codepen.min.css">
+</head>
+
+<style type="text/css">
+   .tox-statusbar__path{
+  visibility:hidden !important;
+}
+.tox-statusbar{
+  visibility:hidden !important;
+}
+.tox-notifications-container{
+   visibility:hidden !important;
+}
+</style>
+<body>
+
+
+<!--Appears in Tiny Blueprint (https://www.tiny.cloud/blog/) in articles:
+ - The TinyMCE WYSIWYG editor is free forever-->
+<textarea id="full-featured-non-premium">
+</textarea>
+
+
+</body>
+</html>
